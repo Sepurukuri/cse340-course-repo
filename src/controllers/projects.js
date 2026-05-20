18c8f6806e5740fc87dea77fd899ec6c16d31061
Santiago Cuadrado <santiagocuadradosud247@gmail.com>
2026-05-20 20:45:29 -0300

    updates

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -1,10 +1,9 @@
-// Import model functions
 import { getAllProjects } from '../models/projects.js';
 
-// Define controller functions
 const showProjectsPage = async (req, res) => {
 
     const projects = await getAllProjects();
+
     const title = 'Community Service Projects';
 
     res.render('projects', {
@@ -13,5 +12,4 @@ const showProjectsPage = async (req, res) => {
     });
 };
 
-// Export controller functions
 export { showProjectsPage };
