30a87367a23ad26cf7f356162f4e9400ce7dea45
Santiago Cuadrado <santiagocuadradosud247@gmail.com>
2026-05-19 22:48:35 -0300

    updates

--- src/models/projects.js
+++ src/models/projects.js
@@ -1,3 +1,24 @@
+import db from './db.js';
+
+const getAllProjects = async () => {
+
+    const query = `
+        SELECT
+            project_id,
+            organization_id,
+            title,
+            description,
+            location,
+            date
+        FROM project
+        ORDER BY date;
+    `;
+
+    const result = await db.query(query);
+
+    return result.rows;
+};
+
 const getProjectsByOrganizationId = async (organizationId) => {
 
     const query = `
