fb2e28f4824f40ee01f5d070a4f8576902213f4a
Santiago Cuadrado <santiagocuadradosud247@gmail.com>
2026-05-28 00:01:23 -0300

    week4

--- src/routes.js
+++ src/routes.js
@@ -14,7 +14,10 @@ import {
 
 import {
     showProjectsPage,
-    showProjectDetailsPage
+    showProjectDetailsPage,
+    showNewProjectForm,
+    processNewProjectForm,
+    projectValidation
 } from './controllers/projects.js';
 
 import {
@@ -40,20 +43,40 @@ router.get('/category/:id', showCategoryDetailsPage);
 
 router.get('/organization/:id', showOrganizationDetailsPage);
 
-// Route to handle new organization form submission
-// Route to handle new organization form submission
-router.post('/new-organization', organizationValidation, processNewOrganizationForm);
-
 // Route for new organization page
 router.get('/new-organization', showNewOrganizationForm);
 
-router.get('/edit-organization/:id', showEditOrganizationForm);
+// Route to handle new organization form submission
+router.post(
+    '/new-organization',
+    organizationValidation,
+    processNewOrganizationForm
+);
 
+// Route to display the edit organization form
+router.get(
+    '/edit-organization/:id',
+    showEditOrganizationForm
+);
+
+// Route to handle the edit organization form submission
 router.post(
     '/edit-organization/:id',
     organizationValidation,
     processEditOrganizationForm
 );
+
+router.post(
+    '/new-project',
+    projectValidation,
+    processNewProjectForm
+);
+
+// Route for new project page
+router.get('/new-project', showNewProjectForm);
+
+// Route to handle new project form submission
+router.post('/new-project', processNewProjectForm);
 
 // Error test route
 router.get('/test-error', testErrorPage);
